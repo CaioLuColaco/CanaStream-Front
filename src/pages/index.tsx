2e64c60d1921021eb9d69db7f00b59fef5b1a53f
caio lucena <caio.lcolaco@gmail.com>
2023-03-21 20:20:09 -0300

    solve some bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from '../components/header'
 import Navbar from '../components/Navbar'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
