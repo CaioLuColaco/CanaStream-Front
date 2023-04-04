52f5b4ea03f016290a8546278e39ed279deb17d2
Nicholas Cabral <nicholasscabral@gmail.com>
2023-04-04 18:40:37 -0300

    add password field on signup form

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,41 +4,58 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/footer";
 
 export default function SignUp() {
-  const [nome, setNome] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+    alert("usuario cadastrado");
   };
 
   return (
     <>
       <Navbar />
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label className={styles.label}>
-          Nome:
-          <input
-            className={styles.input}
-            type="text"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-        </label>
-        <label className={styles.label}>
-          Email:
-          <input
-            className={styles.input}
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <button className={styles.button_submit} type="submit">
-          Cadastrar
-        </button>
-      </form>
+      <div className={styles.form_wrapper}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label}>
+            Nome de usuário
+            <input
+              className={styles.input}
+              placeholder=""
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </label>
+          <label className={styles.label}>
+            Email:
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="insira seu melhor email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          <label className={styles.label}>
+            Senha
+            <input
+              className={styles.input}
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button className={styles.button_submit} type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+
       <Footer />
     </>
   );
