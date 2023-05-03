a063efa0ad89354692e32618faee99c1d2cef955
Nicholas Cabral <nicholasscabral@gmail.com>
2023-05-02 21:29:22 -0300

    feat: changes

--- src/components/Musics/index.tsx
+++ src/components/Musics/index.tsx
@@ -1,25 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from '@/styles/musics.module.css';
-import Link from 'next/link';
-import { api } from '@/pages/api/axios';
-import Modal from '../Modal';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "@/styles/musics.module.css";
+import Link from "next/link";
+import { api } from "@/pages/api/axios";
+import Modal from "../Modal";
 
 export default function Musics() {
   const [playlists, setPlaylists] = useState([]);
   const [musics, setMusics] = useState([]);
   const [filteredMusics, setFilteredMusics] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    api.get('/playlists')
+    api
+      .get("/playlists")
       .then((response) => setPlaylists(response.data))
       .catch((error) => console.log(error));
 
-    api.get('/musics')
+    api
+      .get("/musics")
       .then((response) => setMusics(response.data))
       .catch((error) => console.log(error));
   }, []);
@@ -41,7 +43,7 @@ export default function Musics() {
   };
 
   return (
-    <div style={{ backgroundColor: '#101C27', textDecoration: 'none' }}>
+    <div style={{ backgroundColor: "#101C27", textDecoration: "none" }}>
       <div className={styles.title}>
         <h1>Playlists</h1>
       </div>
@@ -50,7 +52,7 @@ export default function Musics() {
           <div key={playlist.id} className={styles.music}>
             <Link
               href={{
-                pathname: '/musics/',
+                pathname: "/musics/",
                 query: {
                   id: playlist.id,
                   name: playlist.name,
@@ -58,7 +60,7 @@ export default function Musics() {
                   musics: JSON.stringify(playlist.musics),
                 },
               }}
-              style={{ textDecoration: 'none' }}
+              style={{ textDecoration: "none" }}
             >
               <img src={playlist.imgUrl} />
               <h3>{playlist.name}</h3>
@@ -84,14 +86,12 @@ export default function Musics() {
                 onChange={handleSearchTermChange}
               />
             </form>
-            <h1 style={{ color: '#000' }}>Músicas disponíveis</h1>
-            <ul style={{ margin: '0', padding: '0' }}>
+            <h1 style={{ color: "#000" }}>Músicas disponíveis</h1>
+            <ul style={{ margin: "0", padding: "0" }}>
               {filteredMusics.map((music) => (
                 <li key={music.id} className={styles.item2}>
-                  <div>
-                    {music.name}
-                  </div>
-                  <button className={styles.Adicionar}>Adicionar</button>                
+                  <div>{music.name}</div>
+                  <button className={styles.Adicionar}>Adicionar</button>
                 </li>
               ))}
             </ul>
--- src/pages/musics.tsx
+++ src/pages/musics.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import styles from '@/styles/musics.module.css'
-import Navbar from '../components/Navbar'
-import Footer from "@/components/footer"
+import styles from "@/styles/musics.module.css";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/footer";
 import { useRouter } from "next/router";
-import ReactPlayer from 'react-player';
-import { useState } from 'react';
+import ReactPlayer from "react-player";
+import { useState } from "react";
 
 export default function Musics() {
-
   const [playing, setPlaying] = useState(false);
 
   const handlePlay = () => {
@@ -17,33 +16,35 @@ export default function Musics() {
   const handlePause = () => {
     setPlaying(false);
   };
-    
+
   const router = useRouter();
   const { id, name, img, musics } = router.query;
-  const songsObject = typeof musics === 'string' ? JSON.parse(musics) : [];
+  const songsObject = typeof musics === "string" ? JSON.parse(musics) : [];
 
   return (
     <>
-    <main className={styles.main} style={{backgroundColor: '#101C27'}}>
-        <Navbar/>
+      <main className={styles.main} style={{ backgroundColor: "#101C27" }}>
+        <Navbar />
         <div>
-            <div className={styles.Container}>
-              <div>
-                <h1 className={styles.title}>Playlist {name}</h1>
-                <div className={styles.ImgBox}>
-                  <img className={styles.Logo} src={img as string} alt='' />
-                </div>
-              </div>
-              <div style={{width: '50rem'}}>
-                <h1 className={styles.title}>Musicas da playlist {name}</h1>
-                <ul style={{margin: '0', padding: '0'}}>
-                  {songsObject.map((song:any) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <li className={styles.item}>{song.title}<br/>{song.artist}</li>
-                  ))}
-                </ul>                
+          <div className={styles.Container}>
+            <div>
+              <h1 className={styles.title}>Playlist {name}</h1>
+              <div className={styles.ImgBox}>
+                <img className={styles.Logo} src={img as string} alt="" />
               </div>
             </div>
+            <div style={{ width: "50rem" }}>
+              <h1 className={styles.title}>Musicas da playlist {name}</h1>
+              <ul style={{ margin: "0", padding: "0" }}>
+                {songsObject.map((music) => (
+                  <li key={music.id} className={styles.item}>
+                    {music.name}
+                    <br />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
         <Footer />
       </main>
