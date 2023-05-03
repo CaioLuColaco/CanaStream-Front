a4779ab053d06c58c18b6d557788fe9d51de94be
caio lucena <caio.lcolaco@gmail.com>
2023-05-02 21:49:36 -0300

    adjusted the navbar to work

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,24 +2,26 @@ import styles from '@/styles/navbar.module.css'
 import { SessionManager } from '@/utils/session-manager'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Navbar(props) {
-    const isLoggedIn = SessionManager.hasToken()
-    const sessionItemText = isLoggedIn ? "Sair" : "Entrar"
-    const sessionItemPath = isLoggedIn ? "/logout" : "/login"
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    useEffect(() => {
+        setIsLoggedIn(SessionManager.hasToken())
+    })
+
     return (
         <div className={styles.box}>
             <div className={styles.logoBox}>
-                <Image src="/logo.png" width={70} height={70}/>
+                <Image src="/logo.png" alt="logo canastream" width={70} height={70}/>
                 <h1>CanaStream</h1>
             </div>
 
             <div className={styles.boxContent}> 
-                {isLoggedIn && <Link href='/' className={styles.link}>Home</Link>}
+                {isLoggedIn? <Link href='/' className={styles.link}>Home</Link> : ""}
                 <Link href='/faq' className={styles.link}>FAQ</Link>
                 <Link href='/signup' className={styles.link}>Cadastro</Link>
-                <Link href={sessionItemPath} className={styles.link}>{sessionItemText}</Link>
+                {isLoggedIn? <Link href='/logout' className={styles.link}>Sair</Link> : <Link href='/login' className={styles.link}>Entrar</Link>}
             </div>
         </div>
     )
